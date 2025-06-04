d94b93b6b43877e73b3730c36ad58cf8c224bac2
JoelRunevic <joelrunevic@college.harvard.edu>
2025-06-03 21:16:27 -0400

    hard coding a 2-layer MLP

--- mlp.cpp
+++ mlp.cpp
@@ -19,6 +19,13 @@ inline double mat_at(const Mat &m, size_t rows, size_t cols, size_t i, size_t j)
     return m[i * cols + j]; 
 }
 
+// Defining a Cache struct to store values.
+struct Cache {
+    Vec z1; // pre-activation of a hidden layer (d_hidden)
+    Vec a1; // activation of hidden layer (d_hidden)
+    Vec z2;  // pre-activation of output layer (d_out)
+};
+
 
 // Defining the intial MLP struct.
 struct MLP {
@@ -45,9 +52,73 @@ struct MLP {
     {
         assert(d_in > 0 && d_hidden > 0 && d_out > 0);
     }
+
+    // Just applying an affine transformation: z = Ax + b. 
+    // For now, this is hard-coded to two layers and does not have much modularity.
+    Vec forward(const Vec &x, Cache &cache) const {
+        assert(x.size() == d_in);
+
+        // 1) z_{1} = W_{1}x + b_{1}
+        cache.z1.assign(d_hidden, 0.0);
+        for (size_t i = 0; i < d_hidden; ++i){
+            double sum = 0.0;
+            for (size_t j = 0; j < d_in; ++j){
+                sum += mat_at(W1, d_hidden, d_in, i, j) * x[j];
+            }
+            cache.z1[i] = sum + b1[i];
+        }
+
+        // 2) a_{1} = ReLU(z_{1})
+        cache.a1 = cache.z1; 
+        for (size_t i = 0; i < d_hidden; ++i){
+            if (cache.a1[i] < 0){
+                cache.a1[i] = 0;
+            }
+        }
+        
+        // 3) z_{2} = W_{2}a_{1} + b_{2}
+        cache.z2.assign(d_out, 0.0);
+        for (size_t i = 0; i < d_out; ++i){
+            double sum = 0.0; 
+            for (size_t j = 0; j < d_hidden; ++j){
+                sum += mat_at(W2, d_out, d_hidden, i, j) * cache.a1[j];
+            }
+            cache.z2[i] = sum + b2[i];
+        }
+
+        // Equivalent to z_{2} = W_{2}(ReLU(W_{1}x + b_{1})) + b_{2}
+        return cache.z2;
+    }
+
 };
 
 
 int main() {
-    return 0; 
+    // Doing a toy example for correctness.
+    MLP net(2, 3, 1);
+    Cache cache;
+
+    for (double &w : net.W1){
+        w = 1.0;
+    }
+    std::fill(net.b1.begin(), net.b1.end(), 2.0);
+
+    for (double &w : net.W2){
+        w = 1.0; 
+    }
+    std::fill(net.b2.begin(), net.b2.end(), 2.0);
+
+    Vec x = {2.0, 5.0};
+    Vec z2 = net.forward(x, cache);
+
+    std::cout << "z_{1} = ";
+    for (double v : cache.z1) std::cout << v << " ";
+
+    std::cout << "\na_{1} = "; 
+    for (double v : cache.a1) std::cout << v << " ";
+
+    std::cout << "\nz_{2} = ";
+    for (double v : cache.z2) std::cout << v << " ";
+
+    return 0;
 }
